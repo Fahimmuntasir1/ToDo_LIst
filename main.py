8e55901d9e7dd034587138f38b2bf1f7fbf1198b
Fahimmuntasir1 <fahimmuntasir043@gamil.com>
2024-05-03 08:38:46 -0700

    main function greeting intro added

---
+++ main.py
@@ -0,0 +1,16 @@
+# ----Project of ToDo List----
+
+# add a variable
+Task = []
+
+if __name__ == "main":
+    # to run this app create a loop
+    while True:
+        print("\n")
+        print("Select a option to do a operation >>")
+        print("Select 1 to add a task")
+        print("Select 2 to delete/remove a task")
+        print("Select 3 to get all the tasks")
+        print("Select 4 for EXIT")
+
+print("Application running......")
